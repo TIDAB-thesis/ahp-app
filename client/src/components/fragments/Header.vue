10083a3d02798f153999689c48d9d24243fcfdea
Emil Lindholm Brandt <emil93@gmail.com>
2019-11-28 13:20:47 +0100

    Rebranded as Thorleif

--- client/src/components/fragments/Header.vue
+++ client/src/components/fragments/Header.vue
@@ -1,21 +1,21 @@
 <template>
   <header>
-    <h1>Data model-chooser helper X2000™</h1>
+    <h1>Thorleif</h1>
+    <h2>Data-model choice helper</h2>
   </header>
 </template>
-
-<script>
-export default {
-
-}
-</script>
 
 <style scoped>
   h1 {
     margin: 0;
   }
+  h2 {
+    margin: 0;
+    font-style: italic;
+    font-weight: lighter;
+  }
   header {
-    padding: 15px 0;
+    padding: 15px 0 5px;
     background: #82df76;
   }
 </style>
