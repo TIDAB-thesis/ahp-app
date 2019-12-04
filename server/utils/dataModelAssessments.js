f2694e8b1a2ccfff863a7d54fe52b54f61784dfa
Sabina <sabinaha@kth.se>
2019-12-04 08:53:28 +0100

    Changed the data model assessments

--- server/utils/dataModelAssessments.js
+++ server/utils/dataModelAssessments.js
@@ -1,18 +1,18 @@
 const math = require('mathjs');
 module.exports = {
   "manyToMany": [
-    [1, 1, math.fraction(1,5), 3, math.fraction(1,7)],
-    [1, 1, math.fraction(1,5), 3, math.fraction(1,7)],
-    [5, 5, 1, 7, math.fraction(1,4)],
-    [math.fraction(1,3), math.fraction(1,3), math.fraction(1,7), 1, math.fraction(1,9)],
-    [7, 7, 4, 9, 1]
+    [1, 1, math.fraction(1,5), 1, math.fraction(1,9)],
+    [1, 1, math.fraction(1,5), 1, math.fraction(1,9)],
+    [5, 5, 1, 5, math.fraction(1,4)],
+    [1, 1, math.fraction(1,5), 1, math.fraction(1,9)],
+    [9, 9, 4, 9, 1]
   ],
   "oneToMany": [
-    [1, 7, 3, 9, 1],
-    [math.fraction(1,7), 1, math.fraction(1,3), 6, math.fraction(1,7)],
-    [math.fraction(1,3), 3, 1, 8, math.fraction(1,3)],
-    [math.fraction(1,9), math.fraction(1,6), math.fraction(1,8), 1, math.fraction(1,9)],
-    [1, 7, 3, 9, 1]
+    [1, 9, 3, 7, 1],
+    [math.fraction(1,9), 1, math.fraction(1,5), math.fraction(1,3), math.fraction(1,9)],
+    [math.fraction(1,3), 5, 1, 5, math.fraction(1,3)],
+    [math.fraction(1,7), 3, math.fraction(1,5), 1, math.fraction(1,7)],
+    [1, 9, 3, 7, 1]
   ],
   "flexibility": [
     [1, 1, 7, 1, 7],
